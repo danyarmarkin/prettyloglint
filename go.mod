15a7eabe12f8353ac54a303c38ca7ad967519e07
danyarmarkin <danila.iarmarkin@gmail.com>
2026-02-27 19:27:31 +0300

    golangci-lint working plugin

--- go.mod
+++ go.mod
@@ -6,6 +6,8 @@ require golang.org/x/tools v0.42.0 // Используется для созда
 
 require golang.org/x/mod v0.33.0 // indirect
 
+require github.com/golangci/plugin-module-register v0.1.2
+
 require (
 	github.com/google/go-cmp v0.7.0 // indirect
 	golang.org/x/sync v0.19.0 // indirect
--- plugin/module.go
+++ plugin/module.go
@@ -1,19 +1,28 @@
-package main
+package plugin
 
 import (
 	"github.com/danyarmarkin/prettyloglint/internal/analyzer"
+	"github.com/golangci/plugin-module-register/register"
 
 	"golang.org/x/tools/go/analysis"
 )
 
-type analyzerPlugin struct{}
-
-func (*analyzerPlugin) GetAnalyzers() []*analysis.Analyzer {
-	return []*analysis.Analyzer{
-		analyzer.Analyzer,
-	}
+func init() {
+	register.Plugin("prettyloglint", New)
 }
 
-func New(conf any) ([]*analysis.Analyzer, error) {
-	return []*analysis.Analyzer{analyzer.Analyzer}, nil
+type analyzerPlugin struct{}
+
+func (p *analyzerPlugin) BuildAnalyzers() ([]*analysis.Analyzer, error) {
+	return []*analysis.Analyzer{
+		analyzer.Analyzer,
+	}, nil
+}
+
+func (p *analyzerPlugin) GetLoadMode() string {
+	return register.LoadModeSyntax
+}
+
+func New(conf any) (register.LinterPlugin, error) {
+	return &analyzerPlugin{}, nil
 }
